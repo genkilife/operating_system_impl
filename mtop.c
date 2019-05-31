ef8dbc8ffa231a31751aba1dffdf7af089004d12
genkilife <wangyenkai@gmail.com>
2019-05-31 16:34:50 -0700

    finish mtop format

--- mtop.c
+++ mtop.c
@@ -11,7 +11,7 @@ main(int argc, char *argv[])
     exit();
   }
   int avail_mem = memtop();
-  printf(1, "mtop: available memory is %d\n", avail_mem);
+  printf(1, "available memory: %d\n", avail_mem);
 
   if(getmeminfo() != 0){
 	printf(1, "Fail to get memory info\n");
