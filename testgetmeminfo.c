f8e74e43ad0a8ad5cbb870e2e676c8751a75b255
genkilife <wangyenkai@gmail.com>
2019-05-31 16:36:16 -0700

    remove reducdant var

--- testgetmeminfo.c
+++ testgetmeminfo.c
@@ -6,7 +6,6 @@
 int
 main(int argc, char *argv[])
 {
-  int a=0;
   if(argc > 1){
     printf(1, "mtop: doesn't need input argument\n");
     exit();
@@ -21,8 +20,6 @@ main(int argc, char *argv[])
   if(fork() == 0){
 	avail_mem = memtop();
 	printf(1, "mtop: available memory before memtop is %d\n", avail_mem);
-    a = 1;
-	printf(1, "modify variable %d\n", a);
 	getmeminfo();
 	avail_mem = memtop();
 	printf(1, "mtop: available memory after memtop is %d\n", avail_mem);
