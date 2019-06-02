ea6cbebb5a70c75040d34da416c8f129ec24a02f
genkilife <wangyenkai@gmail.com>
2019-06-01 23:45:25 -0700

    add thread join feature

--- mythread.c
+++ mythread.c
@@ -69,10 +69,43 @@ int thread_create(void(*fcn)(void*), void *arg, void*stack){
 
 int thread_join(void){
 
-    // if I have runnable child and it's parent is curprocess
-    // then we switch to other process
+  // if I have runnable child and it's parent is curprocess
+  // then we switch to other process
+  struct proc *p;
+  int havekids, pid;
+  struct proc *curproc = myproc();
+  
+  acquire(&ptable.lock);
+  for(;;){
+    // Scan through table looking for exited children.
+    havekids = 0;
+    for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
+      if(p->parent != curproc)
+        continue;
+      havekids = 1;
+      if(p->state == ZOMBIE){
+        // Found one.
+        pid = p->pid;
+        p->kstack = 0;
+        p->pid = 0;
+        p->parent = 0;
+        p->name[0] = 0;
+        p->killed = 0;
+        p->state = UNUSED;
+        release(&ptable.lock);
+        return pid;
+      }
+    }
 
-	return 0;
+    // No point waiting if we don't have any children.
+    if(!havekids || curproc->killed){
+      release(&ptable.lock);
+      return -1;
+    }
+
+    // Wait for children to exit.  (See wakeup1 call in proc_exit.)
+    sleep(curproc, &ptable.lock);  //DOC: wait-sleep
+  }
 }
 
 int thread_exit(void){
